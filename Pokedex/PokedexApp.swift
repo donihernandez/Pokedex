88c9c1c7170c090cb4cda527e2748d523e428342
Adonai Domingez <donihernandez003@gmail.com>
2023-07-23 16:55:00 +0200

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Doni on 7/23/23.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
